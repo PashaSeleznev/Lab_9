11cb43a30d286cc47c656cc28bde692f0d9d8f25
PashaSeleznev <113536910+PashaSeleznev@users.noreply.github.com>
2023-04-18 20:55:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,47 @@
-import sqlalchemy as db
 import flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = flask.Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:5433@localhost/lab_7'
+db = SQLAlchemy(app)
 
-engine = db.create_engine('postgresql://postgres:5433@localhost/lab_9')
-connection = engine.connect()
 
-metadata = db.MetaData()
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(512), nullable=False)
 
-books = db.Table('books', metadata,
-                 db.Column('author', db.Text),
-                 db.Column('name', db.Text))
-metadata.create_all(engine)
+    def __init__(self, author, books):
+        self.author = author
+        self.books = [
+            Book(text=book) for book in books.split(',')
+        ]
 
-query = db.insert(books).values(author='A.Gruber', name='Mark of Death')
-Result = connection.execute(query)
 
-#output = connection.execute(books.select()).fetchall()
-#print(output)
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(32), nullable=False)
+
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+    author = db.relationship('Author', backref=db.backref('books', lazy=True))
 
 
 @app.route('/', methods=['GET'])
 def hello():
-    return flask.render_template('index.html', messages=books.query.all())
-
+    return flask.render_template('index.html', messages=Author.query.all())
 
 
 @app.route('/add_book', methods=['POST'])
 def add_book():
-    add = flask.request.form['add']
-    submit = flask.request.form['submit']
+    text = flask.request.form['author']
+    tag = flask.request.form['book']
     # messages.append(Message(text, tag))
-    db.session.add(books(add, submit))
+    db.session.add(Author(text, tag))
     db.session.commit()
 
     return flask.redirect(flask.url_for('hello'))
 
+
+with app.app_context():
+    db.create_all()
 app.run()
